53c74bea98e9c53fda59c7a79771299c6b93f44d
AlWasee111 <2405113@ugrad.cse.buet.ac.bd>
2026-04-29 09:57:25 +0600

    Stable version

--- Linear Sorting/CountingSort.cpp
+++ Linear Sorting/CountingSort.cpp
@@ -4,46 +4,50 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-void CountingSort(vector<int>& array);
+vector<int> CountingSort(vector<int>& array);
 
 int main()
 {
-	vector<int> array = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+	vector<int> array = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
 
 	cout << "Before sort: ";
 	for (int i : array)
 		cout << i << " ";
 	cout << endl;
 
-	CountingSort(array);
+	array = CountingSort(array);
 
 	cout << "After sort: ";
 	for (int i : array)
 		cout << i << " ";
 }
 
-void CountingSort(vector<int>& array)
+vector<int> CountingSort(vector<int>& array)
 {
+	vector<int> sorted(array.size(), 0);
 	int k = array[0];
 
 	for (int i = 1; i < array.size(); i++)
 	{
 		k = max(k, array[i]);
 	}
-
-	vector<int> counter (k + 1, 0);
+	vector<int> counter(k + 1, 0);
 
 	for (int i = 0; i < array.size(); i++)
 	{
 		counter[array[i]]++;
 	}
 
-	array.resize(0);
-	for (int i = 0; i < counter.size(); i++)
+	for (int i = 1; i < array.size(); i++)
 	{
-		for (int j = 0; j < counter[i]; j++)
-		{
-			array.push_back(i);
-		}
+		counter[i] += counter[i - 1];
 	}
-} 
+	
+	for (int i = array.size() - 1; i >= 0; i--)
+	{
+		sorted[counter[array[i]] - 1] = array[i];
+		counter[array[i]]--;
+	}
+
+	return sorted;
+}
